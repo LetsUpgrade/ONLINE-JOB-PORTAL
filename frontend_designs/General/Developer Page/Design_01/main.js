2dd382479a824ba1f9953375f6d5de2da77ae1a4
HariSuriya520 <59723129+HariSuriya520@users.noreply.github.com>
2020-10-01 17:21:11 +0530

    Add files via upload (#73)
    The UI looks good!

---
+++ frontend_designs/General/Developer Page/Design_01/main.js
@@ -0,0 +1,62 @@
+const image1 = document.querySelector(".image1");
+const icons1 = document.querySelector(".icons1");
+image1.onclick = function(){
+	this.classList.toggle("active");
+	icon1.classList.toggle("active");
+}
+
+const image2 = document.querySelector(".image2");
+const icons2 = document.querySelector(".icons2");
+image2.onclick = function(){
+	this.classList.toggle("active");
+	icon2.classList.toggle("active");
+}
+
+const image3 = document.querySelector(".image3");
+const icons3 = document.querySelector(".icons3");
+image3.onclick = function(){
+	this.classList.toggle("active");
+	icon3.classList.toggle("active");
+}
+
+const image4 = document.querySelector(".image4");
+const icons4 = document.querySelector(".icons4");
+image4.onclick = function(){
+	this.classList.toggle("active");
+	icon4.classList.toggle("active");
+}
+
+const image5 = document.querySelector(".image5");
+const icons5 = document.querySelector(".icons5");
+image5.onclick = function(){
+	this.classList.toggle("active");
+	icon5.classList.toggle("active");
+}
+
+const image6 = document.querySelector(".image6");
+const icons6 = document.querySelector(".icons6");
+image6.onclick = function(){
+	this.classList.toggle("active");
+	icon6.classList.toggle("active");
+}
+
+const image7 = document.querySelector(".image7");
+const icons7 = document.querySelector(".icons7");
+image7.onclick = function(){
+	this.classList.toggle("active");
+	icon7.classList.toggle("active");
+}
+
+const image8 = document.querySelector(".image8");
+const icons8 = document.querySelector(".icons8");
+image8.onclick = function(){
+	this.classList.toggle("active");
+	icon8.classList.toggle("active");
+}
+
+const image9 = document.querySelector(".image9");
+const icons9 = document.querySelector(".icons9");
+image9.onclick = function(){
+	this.classList.toggle("active");
+	icon9.classList.toggle("active");
+}
